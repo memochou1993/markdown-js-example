e7389cee7cba983dd26879333dc0f58192735335
William Chou <williamchou@kklab.com>
2024-09-23 01:47:13 +0800

    Support code block highlight

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ const text = `## Memo Chou\n
 Hi there 🙋\n
 I'm Memo Chou, a creative developer passionate about Go, PHP, Rust and JavaScript.\n
 Any questions, or want to get involved, please get in touch.\n
-[Click me](https://epoch.epoch.tw) for more details.`;
+[Click me](https://epoch.epoch.tw) for more details.\n
+\`\`\`javascript
+console.log('Hello, World!');
+\`\`\`\n
+`;
 
 document.querySelector('#app').innerHTML = `
   <div>
--- utils/markdown-utils.js
+++ utils/markdown-utils.js
@@ -1,4 +1,6 @@
-import { marked } from 'marked';
+import hljs from 'highlight.js';
+import { Marked } from 'marked';
+import { markedHighlight } from 'marked-highlight';
 import HtmlUtils from './html-utils';
 
 class MarkdownUtils {
@@ -15,12 +17,26 @@ class MarkdownUtils {
    * Converts a markdown string to an HTML string.
    */
   static toHtml(markdown) {
-    const renderer = new marked.Renderer();
-    renderer.link = ({ href, title, text }) => {
-      return `<a href="${href}" title="${title || ''}" target="_blank" rel="noopener noreferrer">${text}</a>`;
+    const marked = new Marked(
+      markedHighlight({
+        langPrefix: 'lang-',
+        highlight(code, lang) {
+          const options = {
+            language: hljs.getLanguage(lang) ? lang : 'javascript',
+          };
+          return hljs.highlight(code, options).value;
+        },
+      }),
+    );
+    const renderer = {
+      link({ href, title, text }) {
+        return `<a href="${href}" title="${title || ''}" target="_blank" rel="noopener noreferrer">${text}</a>`;
+      },
     };
-    return marked(markdown, { renderer });
-  };
+    return marked
+      .use({ renderer })
+      .parse(markdown);
+  }
 }
 
 export default MarkdownUtils;
